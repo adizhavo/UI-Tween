991c32037cc3a2c8f63383b032692e094ea3f872
Adi Zhavo <adi@gram.gs>
2016-04-23 12:24:04 +0300

    Time Property

---
+++ Assets/Improved UI Tween/Scripts/UI Tween/AnimationProperties/Properties/TimeProperty.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class TimeProperty
+{
+    #region Exposed to Editor
+    [SerializeField] private float duration;
+    public float Duration
+    {
+        set
+        {  if (value >= 0)
+            duration = value; }
+        private get
+        { return duration; }
+    }
+
+    public bool UnscaledTime = true;
+    #endregion
+
+    public float GetPercentageDuration(float percentage)
+    {
+        return percentage / duration;
+    }
+}
