5e5febe1ecffa20bb5e43ce61a2646b8bd79b409
Adi Zhavo <adi@gram.gs>
2016-04-23 12:21:33 +0300

    Interface Setup

---
+++ Assets/Improved UI Tween/Scripts/UI Tween/AnimationProperties/Interfaces.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public interface IFrameTicker
+{
+    void Tick(float deltaTime);
+    bool IsUnscaledTicker();
+}
+
+public interface CorePropertyComponent
+{
+    void Initialize(CoreProperty coreProperty);
+}
+
+public interface TweenExecutor : CorePropertyComponent
+{
+    void ExecuteTween(RectTransform animatedRect);
+}
