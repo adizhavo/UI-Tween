8e394e471ad4796d88c165b41be30f4c54aa7062
Adi Zhavo <adi@gram.gs>
2016-04-23 12:23:51 +0300

    Ease Property

---
+++ Assets/Improved UI Tween/Scripts/UI Tween/AnimationProperties/Properties/EaseProperty.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public struct EaseProperty
+{
+    #region EasePropertyValues
+
+    public Vector3 InitialPoint { get { return initialPoint; } }
+
+    public Vector3 FinalPoint { get { return finalPoint; } }
+
+    public AnimationCurve Ease
+    {
+        get
+        {
+            if (ease != null)
+                return ease;
+            else
+                return new AnimationCurve();
+        }
+    }
+
+    #endregion
+
+    private Vector3 initialPoint;
+    private Vector3 finalPoint;
+    private AnimationCurve ease;
+
+    public EaseProperty(Vector3 StartValues, Vector3 EndValues, AnimationCurve EaseIn, AnimationCurve EaseOut, CoreProperty animationCore)
+    {
+        if (EaseIn != null || EaseOut != null)
+        {
+            Debug.Log("EasinIn or EaseOut are null, please check your request.");
+            return;
+        }
+        
+        bool isAnimationOpen = animationCore.IsOpened();
+
+        initialPoint = (isAnimationOpen) ? StartValues : EndValues;
+        finalPoint = (isAnimationOpen) ? EndValues : StartValues;
+        ease = (isAnimationOpen) ? EaseIn : EaseOut;
+    }
+}
