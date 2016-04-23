016f750f46147e97ca306c559c79a5a609bdb911
Adi Zhavo <adi@gram.gs>
2016-04-23 19:25:32 +0300

    Prepare time property for Editor extension

--- Assets/Improved UI Tween/Scripts/UI Tween/AnimationProperties/Properties/TimeProperty.cs
+++ Assets/Improved UI Tween/Scripts/UI Tween/AnimationProperties/Properties/TimeProperty.cs
@@ -4,17 +4,20 @@
 public class TimeProperty
 {
     #region Exposed to Editor
-    [SerializeField] private float duration;
+    [SerializeField] private float duration = 1f;
     public float Duration
     {
         set
-        {  if (value >= 0)
-            duration = value; }
-        private get
+        {
+            if (value >= 0)
+                duration = value;
+        }
+        get
         { return duration; }
     }
 
     public bool UnscaledTime = true;
+
     #endregion
 
     public float GetPercentageDuration(float percentage)
