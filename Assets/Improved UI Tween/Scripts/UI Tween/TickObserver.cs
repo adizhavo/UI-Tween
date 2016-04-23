6007496d099bc4550f7e76b04378ecf65032faaa
Adi Zhavo <adi@gram.gs>
2016-04-23 12:23:19 +0300

    Tick Observer

---
+++ Assets/Improved UI Tween/Scripts/UI Tween/TickObserver.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TickObserver : MonoBehaviour
+{
+    private static TickObserver instance;
+
+    public static TickObserver Instance { get { return instance; } }
+
+    private List<IFrameTicker> subscribers = new List<IFrameTicker>();
+
+    public void Register(IFrameTicker subscriber)
+    {
+        if (subscriber != null)
+            this.subscribers.Add(subscriber);
+    }
+
+    public void UnRegister(IFrameTicker subscriber)
+    {
+        if (subscriber != null)
+            this.subscribers.Remove(subscriber);
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if (subscribers.Count == 0)
+            return;
+        
+        for (int index = 0; index < subscribers.Count; index++)
+        {
+            float deltaTime = (subscribers[index].IsUnscaledTicker()) ? 
+                    Time.unscaledDeltaTime : 
+                    Time.deltaTime;
+                
+            subscribers[index].Tick(deltaTime);
+        }
+    }
+}
